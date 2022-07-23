bf62a45ab111204c29390b7ae86ff4b52db8f3c4
Sumin <tnals2384@gmail.com>
2022-07-23 16:53:31 +0900

    공연 세부 정보 update

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ service_key="66122ee6118e42288de23913ed3f24fb"
 
 params = {  
     'service' : service_key,
-        'stdate' : 20220710,
-        'eddate' : 20220731,
-        'rows' : 100,
-        'cpage': 1
+        'stdate' : 20220101,
+        'eddate' : 20221231,
+        'rows' : 4000,
+        'cpage': 2
 }
+
 res = requests.get(url=url,params=params)
 data = res.text
 jsonString = json.dumps(xmltodict.parse(data), indent=4, ensure_ascii = False)
@@ -33,11 +34,11 @@ for perf in perfosarray:
     title = perf["prfnm"]
     stdate = perf["prfpdfrom"]
     eddate = perf["prfpdto"]
-    genre = perf["genrenm"]
+    genre = perf["genrenm"] 
     concerthall = perf["fcltynm"]
 
     perfo = {'perfoID':perfoID,'title':title, 'stdate':stdate ,'eddate':eddate ,
          'genre':genre, 'concerthall':concerthall}
-    print(perfo)
+    
     db.performance.insert_one(perfo)
     
