7f53f7cdab19d3ccaef1083a4cb15899be67df6c
Sumin <tnals2384@gmail.com>
2022-08-19 16:49:01 +0900

    공연 세부 정보 update

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ import xmltodict
 
 client = MongoClient('localhost', 27017)
 db = client.chatbot
-collection = db.perfor
+collection = db.performance
 
 url = 'http://www.kopis.or.kr/openApi/restful/pblprfr/'
 service_key="66122ee6118e42288de23913ed3f24fb"
@@ -21,20 +21,21 @@ for i in id:
     perfoid= i['perfoID']
 
     data = res.text
-    jsonString = json.dumps(xmltodict.parse(data), indent=4, ensure_ascii = False)
+    jsonString = json.dumps(xmltodict.parse(data),ensure_ascii = False)
 
     perfList = json.loads(jsonString)
     perfos = perfList["dbs"]
-    perf = perfos['db']
+    try:
+        perf = perfos["db"]
+            
+        #poster, 공연 가격, 출연진, 공연시간 받아오기
+        poster = perf["poster"]
+        price = perf["pcseguidance"]
+        cast = perf["prfcast"]
+        time = perf["dtguidance"]
+        collection.update_one({"perfoID":perfoid},{ "$set": {"poster":poster, "price":price
+            , "cast":cast, "time":time}})
+    except:
+        continue
     
-    #poster, 공연 가격, 출연진, 공연시간 받아오기
-    poster = perf["poster"]
-    price = perf["pcseguidance"]
-    cast = perf["prfcast"]
-    time = perf["dtguidance"]
-    collection.update_one({"perfoID":perfoid},{ "$set": {"poster":poster}})
-    collection.update_one({"perfoID":perfoid},{ "$set": {"price":price}})
-    collection.update_one({"perfoID":perfoid},{ "$set": {"cast":cast}})
-    collection.update_one({"perfoID":perfoid},{ "$set": {"time":time}})
-
 
