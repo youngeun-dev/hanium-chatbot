8e5c6b296f4259328a486cf26a63ad75f886d488
zer0silver <duddms0_0@naver.com>
2022-11-14 19:20:08 +0900

    feat: 장르로 질문한 경우

--- template.py
+++ template.py
@@ -173,6 +173,69 @@ def webhook():
         }
         ],
     }
+
+
+    # 장르로 질문했을 때
+    if query_result.get('action') == 'ask.genre':  # 장르로 질문했을 때
+        genre1 = str(query_result.get('parameters').get('genre'))
+
+        title = collection.find(  # db에서 장르에 해당하는 data찾기
+            {"$and": [{"genre": {"$gnr": genre1}}]},
+            {"_id": 0, "title": 1, "poster": 1, "genre": 1}).limit(10)
+
+        for i in title:
+            fulfillmentText = i['title']
+            thumbnail = i['poster']
+            genre = i['genre']
+
+            col = {
+                "thumbnailImageUrl": thumbnail,
+                "imageBackgroundColor": "#FFFFFF",
+                "title": fulfillmentText,
+                "text": genre,
+                "defaultAction": {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/123"
+                },
+                "actions": [
+                    {
+                        "type": "postback",
+                        "label": "More Information",
+                        "data": "action=buy&itemid=111"
+                    },
+                    {
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "http://example.com/page/111"
+                    }
+                ]
+            }
+            column.append(col);
+
+        # print(fulfillmentText)
+        # print(thumbnail)
+        # print(genre)
+        return {
+            "fulfillmentMessages": [
+                {
+                    "payload": {
+                        "line": {
+                            "type": "template",
+                            "altText": "this is a carousel template",
+                            "template": {
+                                "type": "carousel",
+                                "columns": column,
+                                "imageAspectRatio": "rectangle",
+                                "imageSize": "cover"
+                            }
+                        }
+
+                    },
+                    "platform": "LINE"
+                },
+            ]
+        }
     
 if __name__ =='__main__':
     #port = int(os.getenv('PORT',80))
