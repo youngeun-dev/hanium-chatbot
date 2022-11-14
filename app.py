5b931c00d30f6a14565ac220dcd0455db607524f
zer0silver <duddms0_0@naver.com>
2022-11-14 19:24:05 +0900

    feat: 제목으로 질문한 경우

--- app.py
+++ app.py
@@ -236,6 +236,95 @@ def webhook():
                 },
             ]
         }
+
+    # 제목으로 질문했을 때
+    if query_result.get('action') == 'ask.title':
+        result = str(query_result.get('parameters').get('title'))
+
+        perfid = collection.find(  # db에서 제목에 해당하는 data찾기
+            {"title": result})
+
+        for i in perfid:
+            fulfillmentText = "Title:" + i['title'] + ", ID:" + i['perfoID'] + ", Genre: " + i['genre']
+
+        print(fulfillmentText)
+        return {
+            "fulfillmentMessages": [
+                {
+                    "payload": {
+                        "line": {
+                            "type": "template",
+                            "altText": "this is a carousel template",
+                            "template": {
+                                "type": "carousel",
+                                "columns": [
+                                    {
+                                        "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                                        "imageBackgroundColor": "#FFFFFF",
+                                        "title": "this is menu",
+                                        "text": "description",
+                                        "defaultAction": {
+                                            "type": "uri",
+                                            "label": "View detail",
+                                            "uri": "http://example.com/page/123"
+                                        },
+                                        "actions": [
+                                            {
+                                                "type": "postback",
+                                                "label": "Buy",
+                                                "data": "action=buy&itemid=111"
+                                            },
+                                            {
+                                                "type": "postback",
+                                                "label": "Add to cart",
+                                                "data": "action=add&itemid=111"
+                                            },
+                                            {
+                                                "type": "uri",
+                                                "label": "View detail",
+                                                "uri": "http://example.com/page/111"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                                        "imageBackgroundColor": "#000000",
+                                        "title": "this is menu",
+                                        "text": "description",
+                                        "defaultAction": {
+                                            "type": "uri",
+                                            "label": "View detail",
+                                            "uri": "http://example.com/page/222"
+                                        },
+                                        "actions": [
+                                            {
+                                                "type": "postback",
+                                                "label": "Buy",
+                                                "data": "action=buy&itemid=222"
+                                            },
+                                            {
+                                                "type": "postback",
+                                                "label": "Add to cart",
+                                                "data": "action=add&itemid=222"
+                                            },
+                                            {
+                                                "type": "uri",
+                                                "label": "View detail",
+                                                "uri": "http://example.com/page/222"
+                                            }
+                                        ]
+                                    }
+                                ],
+                                "imageAspectRatio": "rectangle",
+                                "imageSize": "cover"
+                            }
+                        }
+
+                    },
+                    "platform": "LINE"
+                },
+            ]
+        };
     
 if __name__ =='__main__':
     #port = int(os.getenv('PORT',80))
